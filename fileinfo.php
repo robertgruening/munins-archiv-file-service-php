0c25f5e725dc51005b04a938ea4be468496727f3
Robert Grüning <r.gruening@web.de>
2020-09-10 17:10:40 +0200

    #2 Dateiinfo-Objekt erzeugt, befüllt und zurückgeschickt

--- fileinfo.php
+++ fileinfo.php
@@ -12,3 +12,13 @@ if (!file_exists($rootPath.$_GET["relativeFilePath"]))
 	http_response_code(404);
 	return;
 }
+
+$fileInfo = array();
+$fileInfo["url"] = "http://10.0.0.1/munins-archiv-file-service/fileInfo/".$_GET["relativeFilePath"];
+$fileInfo["size"] = filesize($rootPath.$_GET["relativeFilePath"]);
+$fileInfo["name"] = basename($rootPath.$_GET["relativeFilePath"]);
+$fileInfo["extension"] = pathinfo($rootPath.$_GET["relativeFilePath"], PATHINFO_EXTENSION);
+$fileInfo["creationDate"] = filectime($rootPath.$_GET["relativeFilePath"]);
+$fileInfo["lastModifiedDate"] = filemtime($rootPath.$_GET["relativeFilePath"]);
+
+echo json_encode($fileInfo); 
