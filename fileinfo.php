ed2f5c31ed6588f9b9513bc7872b0eb6d61a983a
Robert Grüning <r.gruening@web.de>
2020-09-11 11:25:36 +0200

    #2 renamed parameter

--- fileinfo.php
+++ fileinfo.php
@@ -7,18 +7,18 @@ if (!isset($_GET["relativeFilePath"]))
 
 $rootPath = "/srv/samba/AAF/Fundstellen/Akten/";
 
-if (!file_exists($rootPath.$_GET["relativeFilePath"])) 
+if (!file_exists($rootPath.$_GET["relativePath"])) 
 {
 	http_response_code(404);
 	return;
 }
 
 $fileInfo = array();
-$fileInfo["url"] = "http://10.0.0.1/munins-archiv-file-service/fileInfo/".$_GET["relativeFilePath"];
-$fileInfo["size"] = filesize($rootPath.$_GET["relativeFilePath"]);
-$fileInfo["name"] = basename($rootPath.$_GET["relativeFilePath"]);
-$fileInfo["extension"] = pathinfo($rootPath.$_GET["relativeFilePath"], PATHINFO_EXTENSION);
-$fileInfo["creationDate"] = filectime($rootPath.$_GET["relativeFilePath"]);
-$fileInfo["lastModifiedDate"] = filemtime($rootPath.$_GET["relativeFilePath"]);
+$fileInfo["url"] = "http://10.0.0.1/munins-archiv-file-service/fileInfo/".$_GET["relativePath"];
+$fileInfo["size"] = filesize($rootPath.$_GET["relativePath"]);
+$fileInfo["name"] = basename($rootPath.$_GET["relativePath"]);
+$fileInfo["extension"] = pathinfo($rootPath.$_GET["relativePath"], PATHINFO_EXTENSION);
+$fileInfo["creationDate"] = filectime($rootPath.$_GET["relativePath"]);
+$fileInfo["lastModifiedDate"] = filemtime($rootPath.$_GET["relativePath"]);
 
 echo json_encode($fileInfo); 
