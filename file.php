baca0e68daf951baea267bcfa8d8504897fcd6ff
Robert Grüning <r.gruening@web.de>
2020-09-11 11:24:51 +0200

    #1 renamed parameter

--- file.php
+++ file.php
@@ -7,11 +7,11 @@ if (!isset($_GET["relativeFilePath"]))
 
 $rootPath = "/srv/samba/AAF/Fundstellen/Akten/";
 
-if (!file_exists($rootPath.$_GET["relativeFilePath"])) 
+if (!file_exists($rootPath.$_GET["relativePath"])) 
 {
 	http_response_code(404);
 	return;
 }
 
-header(mime_content_type(pathinfo($rootPath.$_GET["relativeFilePath"], PATHINFO_FILENAME)));
-readfile($rootPath.$_GET["relativeFilePath"]);
+header(mime_content_type(pathinfo($rootPath.$_GET["relativePath"], PATHINFO_FILENAME)));
+readfile($rootPath.$_GET["relativePath"]);
