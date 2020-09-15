bdc6ccf1a5f8312b0449c5c482132a7dd0c49d4d
Robert Grüning <r.gruening@web.de>
2020-09-15 10:18:48 +0200

    #5 Konfigurations

--- directoryinfo.php
+++ directoryinfo.php
@@ -1,19 +1,19 @@
 <?php
+include_once(__DIR__."/config.php");
+
 if (!isset($_GET["relativePath"]))
 {
 	http_response_code(404);
 	return;
 }
 
-$rootPath = "/srv/samba/AAF/Fundstellen/Akten/";
-
-if (!file_exists($rootPath.$_GET["relativePath"])) 
+if (!file_exists(ROOT_DIR.$_GET["relativePath"])) 
 {
 	http_response_code(404);
 	return;
 }
 
-if (!is_dir($rootPath.$_GET["relativePath"])) 
+if (!is_dir(ROOT_DIR.$_GET["relativePath"])) 
 {
 	http_response_code(404);
 	return;
@@ -21,7 +21,7 @@ if (!is_dir($rootPath.$_GET["relativePath"]))
 
 $dirInfo = array();
 $dirInfo["relativePath"] = $_GET["relativePath"];
-$dirInfo["creationDate"] = filectime($rootPath.$_GET["relativePath"]);
-$dirInfo["lastModifiedDate"] = filemtime($rootPath.$_GET["relativePath"]);
+$dirInfo["creationDate"] = filectime(ROOT_DIR.$_GET["relativePath"]);
+$dirInfo["lastModifiedDate"] = filemtime(ROOT_DIR.$_GET["relativePath"]);
 
 echo json_encode($dirInfo);
