86288f21acc1fe3aeabe2079c998144b63ed9bc6
Robert Grüning <r.gruening@web.de>
2020-09-15 10:20:11 +0200

    #5 Konfigurationsdatei eingebunden

--- file.php
+++ file.php
@@ -1,17 +1,17 @@
 <?php
+include_once(__DIR__."/config.php");
+
 if (!isset($_GET["relativePath"]))
 {
 	http_response_code(404);
 	return;
 }
 
-$rootPath = "/srv/samba/AAF/Fundstellen/Akten/";
-
-if (!file_exists($rootPath.$_GET["relativePath"])) 
+if (!file_exists(ROOT_DIR.$_GET["relativePath"])) 
 {
 	http_response_code(404);
 	return;
 }
 
-header(mime_content_type(pathinfo($rootPath.$_GET["relativePath"], PATHINFO_FILENAME)));
-readfile($rootPath.$_GET["relativePath"]);
+header(mime_content_type(pathinfo(ROOT_DIR.$_GET["relativePath"], PATHINFO_FILENAME)));
+readfile(ROOT_DIR.$_GET["relativePath"]);
